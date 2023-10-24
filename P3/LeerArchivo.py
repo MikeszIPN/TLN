bf6d5c068e80d4a24f2485157c681e29915a8d20
NavilPinedaRugerio <navyadenip@gmail.com>
2023-10-24 12:37:33 -0600

    Archivo

--- P3/LeerArchivo.py
+++ P3/LeerArchivo.py
@@ -1,15 +1,14 @@
-# Leer el archivo de texto
 import numpy as np
 
 def load_data():
     data = []
     with open("Texto1.txt", "r", encoding='utf-8') as datos:
-        oraciones = []
         for linea in datos:
-            oraciones.append([x for x in linea.strip().split(".")])
-            
-        # Crear matriz para guardar las oraciones
-        data.append(oraciones)
+            oracion = [x for x in linea.strip().split("\n")]
+            data.extend(oracion)  # Cambiar append a extend para agregar las oraciones individuales
         
     print("Se cargaron los datos")
-    return data
+    return data  # Unir todas las oraciones en una sola cadena
+
+documentos = load_data()
+print(documentos)
