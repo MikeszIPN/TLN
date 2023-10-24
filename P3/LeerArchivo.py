8d198b25d491e4e5bed60c57aad34bda1d0254ba
ValeriaJahzeel <chj068378@gmail.com>
2023-10-24 12:43:11 -0600

    ejercicio 2

--- P3/LeerArchivo.py
+++ P3/LeerArchivo.py
@@ -1,9 +1,9 @@
 # Leer el archivo de texto
 import numpy as np
 
-def load_data():
+def load_data(texto):
     data = []
-    with open("Texto1.txt", "r", encoding='utf-8') as datos:
+    with open(texto, "r", encoding='utf-8') as datos:
         oraciones = []
         for linea in datos:
             oraciones.append([x for x in linea.strip().split(".")])
@@ -13,3 +13,5 @@ def load_data():
         
     print("Se cargaron los datos")
     return data
+
+datos = load_data("Texto2.txt")
